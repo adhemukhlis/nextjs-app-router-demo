22667a826746696ba2b16133faf68e307726980f
adhemukhlis <adhemukhlis@gmail.com>
2023-12-06 12:18:21 +0700

    init project

--- next.config.js
+++ next.config.js
@@ -1,4 +1,33 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+	enabled: process.env.ANALYZE === 'true'
+})
+
+const nextConfig = withBundleAnalyzer({
+	publicRuntimeConfig: {
+		AppName: 'NextJS App Router Demo'
+	},
+	env: {
+		// BACKEND_API_HOST: process.env.NEXT_BACKEND_API_HOST
+	},
+	eslint: {
+		dirs: ['.']
+	},
+	poweredByHeader: false,
+	trailingSlash: false,
+	reactStrictMode: false,
+	images: {
+		domains: ['dataflow.wiratek.id'],
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: '**',
+				port: '',
+				pathname: '**'
+			}
+		]
+	}
+	// transpilePackages: ['antd']
+})
 
 module.exports = nextConfig
